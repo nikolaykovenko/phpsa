09865e24e14cc0e28d4e82cc1400a7072a2b5cfc
Nikolay Kovenko <nickk@templatemonster.me>
2016-10-12 15:56:47 +0300

    Added for multiple conditions analyzer

---
+++ tests/analyze-fixtures/Statement/ForCondition.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Analyze\Fixtures\Statement;
+
+class ForCondition
+{
+
+    public function testMultipleConditions()
+    {
+        for ($i=0,$j=0; $i<3,$j<2; $i++,$j++) {
+            echo "test"; // 2 times
+        }
+    }
+
+    public function testNormal()
+    {
+        for ($i=0; $i<3; $i++) {
+            echo "test"; // 1 time
+        }
+    }
+}
+?>
+----------------------------
+[
+    {
+        "type": "for_condition",
+        "message": "You should merge the conditions into one with &&",
+        "file": "ForCondition.php",
+        "line": 9
+    }
+]
